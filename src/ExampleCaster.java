3d8d89ec85b33ca587c383da11b4cfbb9fae3f48
Xin Han <hh235206661@gmail.com>
2015-02-11 22:43:55 +0100

    Update

--- src/ExampleCaster.java
+++ src/ExampleCaster.java
@@ -1,6 +1,12 @@
 
 import mcgui.*;
 
+import java.util.UUID;
+import java.util.LinkedList;
+import java.util.List;
+import java.io.*;
+
+
 /**
  * Simple example of how to use the Multicaster interface.
  *
@@ -8,11 +14,20 @@ import mcgui.*;
  */
 public class ExampleCaster extends Multicaster {
 
+	int Rg = 0;
+	int Sg = 0;
+	/* assume that no.0 node is the sequencer */
+	int sequencer_id = 0;
+	// hold_back_queue
+	final List<String> hold_back_queue = new LinkedList<String>();
+	//UUID uuid = UUID.randomUUID();
+	
+	
     /**
      * No initializations needed for this simple one
      */
     public void init() {
-        mcui.debug("The network has "+hosts+" hosts!~~~");
+        mcui.debug("The network has "+hosts+" hosts!");
     }
         
     /**
@@ -22,11 +37,14 @@ public class ExampleCaster extends Multicaster {
         for(int i=0; i < hosts; i++) {
             /* Sends to everyone except itself */
             if(i != id) {
+            	messagetext = messagetext+':'+UUID.randomUUID();
                 bcom.basicsend(i,new ExampleMessage(id, messagetext));
             }
         }
         mcui.debug("Sent out: \""+messagetext+"\"");
-        mcui.deliver(id, messagetext, "from myself! my id is:"+getId());
+        // Add message to the hold-back queue
+        hold_back_queue.add(messagetext);
+        //mcui.deliver(id, messagetext, "from myself!");
     }
     
     /**
@@ -34,7 +52,20 @@ public class ExampleCaster extends Multicaster {
      * @param message  The message received
      */
     public void basicreceive(int peer,Message message) {
-        mcui.deliver(peer, ((ExampleMessage)message).text);
+    	// check if the current node is the sequencer
+    	if(sequencer_id == getId()){
+    		mcui.debug("I'm the SEQUENCER!");
+    		String messagetext = ((ExampleMessage)message).text;
+    		String[] unique_id = ((ExampleMessage)message).text.split(":");
+    		System.out.println("--"+unique_id[0]);
+    		System.out.println("--"+unique_id[1]);
+    		System.out.println("--"+unique_id[2]);
+    	}
+    	else{
+    		mcui.deliver(peer, ((ExampleMessage)message).text);
+    	}
+    	
+        //mcui.deliver(peer, ((ExampleMessage)message).text);
     }
 
     /**
@@ -46,4 +77,12 @@ public class ExampleCaster extends Multicaster {
     public void basicpeerdown(int peer) {
         mcui.debug("Peer "+peer+" has been dead for a while now!");
     }
+    
+    
+ 
+    
+    
 }
+
+
+
